85c0c0cac0d078e49d7d38699ead6266ebf298f3
Pierre Brito <carlospierre07@gmail.com>
2023-06-01 15:14:17 -0300

    Body's request

--- getSalesProduct.js
+++ getSalesProduct.js
@@ -2,3 +2,16 @@ require('dotenv').config()
 
 const APP_KEY_SEGCOMP = process.env.APP_KEY_SEGCOMP
 const APP_SECRET_SEGCOMP = process.env.APP_SECRET_SEGCOMP
+
+const link = "https://app.omie.com.br/api/v1/produtos/pedido/"
+const body = (orderNumber) => {
+    return {
+        "call":"ConsultarPedido",
+        "app_key": APP_KEY_SEGCOMP,
+        "app_secret":APP_SECRET_SEGCOMP,
+        "param":
+            [
+                {"numero_pedido":orderNumber}
+            ]
+        }
+    }
